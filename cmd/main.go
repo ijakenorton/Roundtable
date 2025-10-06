d2eef7a708e9e7a9961b8f75f5d119bb54d98bbf
Hayden McAlister <haydenmcalister49@gmail.com>
2025-10-06 21:27:29 +1300

    Add initial main method to load config and set logger

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/hmcalister/roundtable/cmd/config"
+)
+
+func main() {
+	configFilePath := flag.String("configFilePath", "config.yaml", "Set the file path to the config file.")
+	flag.Parse()
+
+	config.LoadConfig(*configFilePath)
+	logFilePointer := config.ConfigureLogger()
+	if logFilePointer != nil {
+		defer logFilePointer.Close()
+	}
+}
