fb2d748aeda312f9c3f4299af9f6031fca742ef3
Hayden McAlister <haydenmcalister49@gmail.com>
2025-10-06 22:34:28 +1300

    Add a basic proof of concept connection between peers with manual connection setup

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"fmt"
+	"log/slog"
+	"time"
 
 	"github.com/hmcalister/roundtable/cmd/config"
+	"github.com/pion/webrtc/v4"
+	"github.com/spf13/viper"
 )
 
 func main() {
@@ -15,4 +21,155 @@ func main() {
 	if logFilePointer != nil {
 		defer logFilePointer.Close()
 	}
+
+	// --------------------------------------------------------------------------------
+
+	webrtcServer := webrtc.ICEServer{
+		URLs: viper.GetStringSlice("ICEServers"),
+	}
+
+	webrtcConfig := webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{webrtcServer},
+	}
+
+	peerOne, errOne := webrtc.NewPeerConnection(webrtcConfig)
+	peerTwo, errTwo := webrtc.NewPeerConnection(webrtcConfig)
+	if err := errors.Join(errOne, errTwo); err != nil {
+		slog.Error("error when creating peer connection",
+			"err", err,
+			"webrtcConfig", webrtcConfig,
+		)
+		panic(err)
+	}
+
+	// --------------------------------------------------------------------------------
+
+	peerOne.OnConnectionStateChange(func(pcs webrtc.PeerConnectionState) {
+		slog.Info(
+			"peer one connection state change",
+			"peer connection state", pcs.String(),
+		)
+	})
+
+	peerTwo.OnConnectionStateChange(func(pcs webrtc.PeerConnectionState) {
+		slog.Info(
+			"peer two connection state change",
+			"peer connection state", pcs.String(),
+		)
+	})
+
+	// --------------------------------------------------------------------------------
+
+	dataChannelOptions := &webrtc.DataChannelInit{}
+
+	dataChannel, err := peerOne.CreateDataChannel("pingChannel", dataChannelOptions)
+	if err != nil {
+		slog.Error("error when creating data channel",
+			"err", err,
+			"dataChannelOptions", dataChannelOptions,
+		)
+		panic(err)
+	}
+
+	dataChannel.OnOpen(func() {
+		slog.Info("peer one opened data channel")
+		go func() {
+			for i := 0; ; i += 1 {
+				msg := fmt.Sprintf("Ping %d", i)
+				slog.Info("peer one sending ping", "msg", msg)
+				if err := dataChannel.SendText(msg); err != nil {
+					slog.Error("error when sending ping", "err", err)
+				}
+				time.Sleep(time.Second)
+			}
+		}()
+	})
+
+	dataChannel.OnMessage(func(msg webrtc.DataChannelMessage) {
+		slog.Info("peer one received", "message", string(msg.Data))
+	})
+
+	peerTwo.OnDataChannel(func(dc *webrtc.DataChannel) {
+		slog.Info("peer two received data channel",
+			"data channel label", dc.Label(),
+			"data channel ID", dc.ID(),
+		)
+
+		dc.OnMessage(func(msg webrtc.DataChannelMessage) {
+			slog.Info("peer two received", "msg", string(msg.Data))
+			reply := fmt.Sprintf("pong %s", string(msg.Data))
+			slog.Info("peer two sending", "reply", reply)
+			dc.SendText(reply)
+		})
+	})
+
+	// --------------------------------------------------------------------------------
+
+	offerOptions := &webrtc.OfferOptions{}
+	answerOptions := &webrtc.AnswerOptions{}
+
+	slog.Info("creating offer")
+	offer, err := peerOne.CreateOffer(offerOptions)
+	if err != nil {
+		slog.Error(
+			"error when creating offer",
+			"err", err,
+			"offerOptions", offerOptions,
+		)
+		panic(err)
+	}
+
+	if err = peerOne.SetLocalDescription(offer); err != nil {
+		slog.Error(
+			"error when setting local description of offer",
+			"err", err,
+			"offer", offer,
+		)
+		panic(err)
+	}
+
+	<-webrtc.GatheringCompletePromise(peerOne)
+
+	if err = peerTwo.SetRemoteDescription(*peerOne.LocalDescription()); err != nil {
+		slog.Error(
+			"error when setting remote description of offer",
+			"err", err,
+		)
+		panic(err)
+	}
+
+	slog.Info("creating answer")
+	answer, err := peerTwo.CreateAnswer(answerOptions)
+	if err != nil {
+		slog.Error(
+			"error when creating answer",
+			"err", err,
+			"answerOptions", answerOptions,
+		)
+		panic(err)
+	}
+
+	if err = peerTwo.SetLocalDescription(answer); err != nil {
+		slog.Error(
+			"error when setting local description of answer",
+			"err", err,
+			"answer", answer,
+		)
+		panic(err)
+	}
+
+	<-webrtc.GatheringCompletePromise(peerTwo)
+
+	if err = peerOne.SetRemoteDescription(*peerTwo.LocalDescription()); err != nil {
+		slog.Error(
+			"error when setting remote description of answer",
+			"err", err,
+		)
+		panic(err)
+	}
+
+	// --------------------------------------------------------------------------------
+
+	// Keep process alive for pings to pass
+	select {}
 }
--- go.mod
+++ go.mod
@@ -7,14 +7,34 @@ require github.com/spf13/viper v1.21.0
 require (
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/pion/datachannel v1.5.10 // indirect
+	github.com/pion/dtls/v3 v3.0.7 // indirect
+	github.com/pion/ice/v4 v4.0.10 // indirect
+	github.com/pion/interceptor v0.1.41 // indirect
+	github.com/pion/logging v0.2.4 // indirect
+	github.com/pion/mdns/v2 v2.0.7 // indirect
+	github.com/pion/randutil v0.1.0 // indirect
+	github.com/pion/rtcp v1.2.15 // indirect
+	github.com/pion/rtp v1.8.23 // indirect
+	github.com/pion/sctp v1.8.39 // indirect
+	github.com/pion/sdp/v3 v3.0.16 // indirect
+	github.com/pion/srtp/v3 v3.0.8 // indirect
+	github.com/pion/stun/v3 v3.0.0 // indirect
+	github.com/pion/transport/v3 v3.0.8 // indirect
+	github.com/pion/turn/v4 v4.1.1 // indirect
+	github.com/pion/webrtc/v4 v4.1.5 // indirect
 	github.com/sagikazarmark/locafero v0.11.0 // indirect
 	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
 	github.com/spf13/afero v1.15.0 // indirect
 	github.com/spf13/cast v1.10.0 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/wlynxg/anet v0.0.5 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
-	golang.org/x/sys v0.29.0 // indirect
-	golang.org/x/text v0.28.0 // indirect
+	golang.org/x/crypto v0.42.0 // indirect
+	golang.org/x/net v0.44.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
 )
