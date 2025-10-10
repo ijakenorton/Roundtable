86593b6199242fa8d08f6818ade1b0fb10ff6a84
Hayden McAlister <haydenmcalister49@gmail.com>
2025-10-10 23:44:36 +1300

    Add default value for localPort config field and correct fields to lowercase
    Move client config into client dir

--- cmd/client/config/config.go
+++ cmd/client/config/config.go
@@ -12,6 +12,7 @@ func LoadConfig(configFilePath string) {
 	// Logging values
 	viper.SetDefault("loglevel", "info")
 	viper.SetDefault("logfile", "")
+	viper.SetDefault("localport", 1066)
 
 	viper.SetConfigFile(configFilePath)
 	if err := viper.ReadInConfig(); err != nil {
