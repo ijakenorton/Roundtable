3b625da76e1a319fbdf93772a3466413dca3d789
Hayden McAlister <haydenmcalister49@gmail.com>
2025-10-11 03:41:59 +1300

    Fix several errors around reading request bodies more than once

--- cmd/signallingserver/main.go
+++ cmd/signallingserver/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"flag"
@@ -28,17 +29,32 @@ func handleSignalOffer(w http.ResponseWriter, r *http.Request) {
 	)
 	requestLogger.Debug("new incoming session offer")
 
-	var signallingOffer networking.SignallingOffer
-	if err := json.NewDecoder(r.Body).Decode(&signallingOffer); err != nil {
+	// TODO: Likely a security risk to read the body... what if the body is very large?
+	requestBody, err := io.ReadAll(r.Body)
+	if err != nil {
 		requestLogger.Error(
-			"error while decoding new session offer from JSON",
+			"error while reading request body",
 			"err", err,
 			"request", r,
 		)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+	slog.Debug("request body", "body", requestBody)
+
+	var signallingOffer networking.SignallingOffer
+	if err := json.Unmarshal(requestBody, &signallingOffer); err != nil {
+		requestLogger.Error(
+			"error while decoding new session offer from JSON",
+			"err", err,
+			"request", r,
+			"requestBody", requestBody,
+		)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 	requestLogger.With("offerUUID", signallingOffer.OfferUUID.String())
+	requestLogger.Debug("received signalling offer", "signallingOffer", signallingOffer)
 
 	// --------------------------------------------------------------------------------
 	// Forward this offer on to the specified remote endpoint (if possible)
@@ -51,7 +67,7 @@ func handleSignalOffer(w http.ResponseWriter, r *http.Request) {
 		ctx,
 		http.MethodPost,
 		remoteEndpointToURL(signallingOffer.RemoteEndpoint),
-		r.Body,
+		bytes.NewReader(requestBody),
 	)
 	if err != nil {
 		requestLogger.Error(
@@ -81,7 +97,7 @@ func handleSignalOffer(w http.ResponseWriter, r *http.Request) {
 	// TODO: Can we avoid reading the answer?
 
 	// TODO: Security of this? What if malicious answeringResponseBody is very large?
-	answeringResponseBody, err := io.ReadAll(req.Body)
+	answeringResponseBody, err := io.ReadAll(resp.Body)
 	if err != nil {
 		requestLogger.Error(
 			"error while reading answering request body",
@@ -90,6 +106,7 @@ func handleSignalOffer(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+	slog.Debug("answering response", "answeringResponseBody", answeringResponseBody)
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
@@ -111,7 +128,7 @@ func main() {
 	// --------------------------------------------------------------------------------
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/signal", handleSignalOffer)
+	mux.HandleFunc("POST /signal", handleSignalOffer)
 	listenAddress := viper.GetString("localaddress")
 	slog.Debug("starting signalling server listening", "listenAddress", listenAddress)
 	if err := http.ListenAndServe(listenAddress, mux); err != nil {
