3f71df53d507a88c3c7c84eb50fa7372dc7a15be
Hayden McAlister <haydenmcalister49@gmail.com>
2025-10-11 01:10:45 +1300

    Add signalling logic to signallingserver

--- cmd/signallingserver/main.go
+++ cmd/signallingserver/main.go
@@ -1,16 +1,101 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"flag"
+	"fmt"
+	"io"
 	"log/slog"
 	"net/http"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/hmcalister/roundtable/cmd/signallingserver/config"
+	"github.com/hmcalister/roundtable/internal/networking"
 	"github.com/spf13/viper"
 )
 
-func handleSignalOffer(w http.ResponseWriter, r *http.Request) {
+func remoteEndpointToURL(remoteEndpoint string) string {
+	// TODO: technically remoteEndpoint is user-defined data,
+	// so this should be validated before using for sprintf...?
+	return fmt.Sprintf("%s/signal", remoteEndpoint)
+}
 
+func handleSignalOffer(w http.ResponseWriter, r *http.Request) {
+	requestLogger := slog.Default().WithGroup("request").With(
+		"requestUUID", uuid.New().String(),
+	)
+	requestLogger.Debug("new incoming session offer")
+
+	var signallingOffer networking.SignallingOffer
+	if err := json.NewDecoder(r.Body).Decode(&signallingOffer); err != nil {
+		requestLogger.Error(
+			"error while decoding new session offer from JSON",
+			"err", err,
+			"request", r,
+		)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	requestLogger.With("offerUUID", signallingOffer.OfferUUID.String())
+
+	// --------------------------------------------------------------------------------
+	// Forward this offer on to the specified remote endpoint (if possible)
+
+	ctx := context.Background()
+	ctx, cancelFunc := context.WithTimeout(ctx, viper.GetDuration("timeout")*time.Second)
+	defer cancelFunc()
+
+	req, err := http.NewRequestWithContext(
+		ctx,
+		http.MethodPost,
+		remoteEndpointToURL(signallingOffer.RemoteEndpoint),
+		r.Body,
+	)
+	if err != nil {
+		requestLogger.Error(
+			"error while creating new http request",
+			"err", err,
+		)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	// If ctx.cancel is called, or ctx timeout is reached, this returns a non-nil error
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		requestLogger.Error(
+			"error while posting offer to remote client",
+			"err", err,
+		)
+		w.WriteHeader(http.StatusServiceUnavailable)
+		return
+	}
+	defer resp.Body.Close()
+	requestLogger.Debug("response received from remote client")
+
+	// --------------------------------------------------------------------------------
+	// Read response from answering client and forward this back to offering client
+	// TODO: Can we avoid reading the answer?
+
+	// TODO: Security of this? What if malicious answeringResponseBody is very large?
+	answeringResponseBody, err := io.ReadAll(req.Body)
+	if err != nil {
+		requestLogger.Error(
+			"error while reading answering request body",
+			"err", err,
+		)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(answeringResponseBody)
+
+	requestLogger.Debug("request fulfilled")
 }
 
 func main() {
@@ -29,5 +114,7 @@ func main() {
 	mux.HandleFunc("/signal", handleSignalOffer)
 	listenAddress := viper.GetString("localaddress")
 	slog.Debug("starting signalling server listening", "listenAddress", listenAddress)
-	http.ListenAndServe(listenAddress, mux)
+	if err := http.ListenAndServe(listenAddress, mux); err != nil {
+		slog.Error("error during listen and serve", "err", err)
+	}
 }
