39ca845279f7c17fb90c004ba1aedb54fbc16dad
Hayden McAlister <haydenmcalister49@gmail.com>
2025-10-13 02:50:57 +1300

    Add audio processing to peer, sending and receiving audio data from PeerConnection

--- internal/peer/peer.go
+++ internal/peer/peer.go
@@ -1,11 +1,13 @@
 package peer
 
 import (
+	"context"
 	"log/slog"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/pion/webrtc/v4"
+	"github.com/pion/webrtc/v4/pkg/media"
 )
 
 const (
@@ -23,8 +25,31 @@ type Peer struct {
 
 	uuid uuid.UUID
 
+	// Audio input data from this client is passed along this channel.
+	// This data is already OPUS encoded by the AudioManager, and converted to media.Samples
+	audioInputDataChannel <-chan media.Sample
+
+	// When finished, disconnected, or otherwise invalidated, we need to tell
+	// the AudioManager we don't want more data by calling this cancel function.
+	audioInputDataChannelCancelFunc context.CancelFunc
+
+	// Audio output data from this client is passed along this channel
+	// to be played on the audio output device.
+	// The Peer must extract the media.Sample.Data to send along this channel.
+	// When done, just close the channel.
+	audioOutputDataChannel chan<- []byte
+
 	// Handles the connection between this client and the remote, peer client
 	connection *webrtc.PeerConnection
+
+	// WebRTC track for sending audio from this client to the remote client
+	connectionAudioInputTrack *webrtc.TrackLocalStaticSample
+}
+
+func (peer *Peer) gracefulShutdown() {
+	peer.audioInputDataChannelCancelFunc()
+	close(peer.audioOutputDataChannel)
+	peer.connection.Close()
 }
 
 // handle a new message on the heartbeat data channel
@@ -59,5 +84,15 @@ func (peer *Peer) heartbeatSendMessageHandler(dc *webrtc.DataChannel) {
 		if err := dc.Send(msg); err != nil {
 			slog.Error("error when sending heartbeat", "err", err)
 		}
+	}
+}
+
+// Handle audio input along the audioInputDataChannel by forwarding through the PeerConnection audio track
+// This method blocks waiting for data on the audioInputDataChannel, so run in a goroutine
+func (peer *Peer) sendAudioInputHandler() {
+	for nextSample := range peer.audioInputDataChannel {
+		// Forward the input data along the connection
+		peer.connectionAudioInputTrack.WriteSample(nextSample)
+
 	}
 }
