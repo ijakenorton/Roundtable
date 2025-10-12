35034396ceff7e6f6fd48008e77ca43ff0a189ab
Hayden McAlister <haydenmcalister49@gmail.com>
2025-10-13 02:51:49 +1300

    Add audio processing to peerfactory, initalizing and starting audio sending/receiving

--- internal/peer/peerfactory.go
+++ internal/peer/peerfactory.go
@@ -1,21 +1,37 @@
 package peer
 
 import (
+	"context"
 	"fmt"
 	"log/slog"
 
 	"github.com/google/uuid"
 	"github.com/pion/webrtc/v4"
+	"github.com/pion/webrtc/v4/pkg/media"
 )
 
 type PeerFactory struct {
-	logger                       *slog.Logger
+	logger *slog.Logger
+
 	audioTrackRTPCodecCapability webrtc.RTPCodecCapability
+
+	// Function that creates a new audio input listener (see github.com/hmcalister/roundtable/internal/audio)
+	// Raw audio input data will arrive along the returned channel.
+	// When a peer disconnects, the returned context.CancelFunction should be called, which stops data being sent along this channel.
+	addAudioInputListenerFunc func() (chan<- media.Sample, context.CancelFunc)
 }
 
 // Create a new PeerFactory.
 //
+// Function that creates a new audio input listener (see github.com/hmcalister/roundtable/internal/audio)
+// Raw audio input data will arrive along the returned channel.
+// When a peer disconnects, the returned context.CancelFunction should be called, which stops data being sent along this channel.
+// This function is give by `AudioManager.AddInputListener()`. We do not pass in the AudioManager directly
+// to ensure decoupling and prevent import cycles.
+//
 // audioTrackRTPCodecCapability defines the configuration to use for all audio tracks created on peer connections.
+// Effectively says what the microphone input sample rate/channels are.
+// Should be taken from the AudioManager for accurate information.
 // See https://github.com/pion/webrtc for details on these options.
 //
 // logger allows for a child logger to be used specifically for this client. Create a child logger like:
@@ -29,6 +45,7 @@ type PeerFactory struct {
 // If no logger is given, slog.Default() is used.
 func NewPeerFactory(
 	audioTrackRTPCodecCapability webrtc.RTPCodecCapability,
+	addAudioInputListenerFunc func() (chan<- media.Sample, context.CancelFunc),
 	logger *slog.Logger,
 ) *PeerFactory {
 	if logger == nil {
@@ -38,6 +55,7 @@ func NewPeerFactory(
 	factory := &PeerFactory{
 		logger:                       logger,
 		audioTrackRTPCodecCapability: audioTrackRTPCodecCapability,
+		addAudioInputListenerFunc:    addAudioInputListenerFunc,
 	}
 
 	return factory
@@ -54,10 +72,12 @@ func (factory *PeerFactory) commonConnectionSetup(peer *Peer) {
 			peer.logger.Info("peer connection disconnected")
 			// TODO: Handle disconnected connection
 			// Should close peer.connection, somehow signal this peer is to be discarded
+			peer.gracefulShutdown()
 		case webrtc.PeerConnectionStateClosed:
 			peer.logger.Info("peer connection closed")
 			// TODO: Handle closed connection
 			// Same as disconnected, close peer.connection, signal discard
+			peer.gracefulShutdown()
 		}
 	})
 
@@ -67,7 +87,18 @@ func (factory *PeerFactory) commonConnectionSetup(peer *Peer) {
 			"track ID", tr.ID(),
 			"track kind", tr.Kind().String(),
 		)
+		go func() {
+			for {
+				pkt, _, err := tr.ReadRTP()
+				if err != nil {
+					peer.logger.Error("error while receiving data from remote peer", "err", err)
+					continue
+				}
 
+				// TODO: Handle dropped and out-of-order packets?
+				peer.audioOutputDataChannel <- pkt.Payload
+			}
+		}()
 	})
 }
 
@@ -128,6 +159,8 @@ func (factory *PeerFactory) NewOfferingPeer(connection *webrtc.PeerConnection) (
 		factory.logger.Error("error while adding audio track to peer connection", "err", err)
 		return nil, err
 	}
+	peer.connectionAudioInputTrack = track
+	go peer.sendAudioInputHandler()
 
 	return peer, nil
 }
@@ -166,6 +199,8 @@ func (factory *PeerFactory) NewAnsweringPeer(connection *webrtc.PeerConnection) 
 		factory.logger.Error("error while adding audio track to peer connection", "err", err)
 		return nil, err
 	}
+	peer.connectionAudioInputTrack = track
+	go peer.sendAudioInputHandler()
 
 	return peer, nil
 }
