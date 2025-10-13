26317d81798f25efbfa1748f622deec1bcf0f46f
Hayden McAlister <haydenmcalister49@gmail.com>
2025-10-14 00:55:44 +1300

    Shift encoderdecoder into separate package, removing poorly-named audio package

--- internal/audio/encoderdecoder.go
+++
@@ -1,9 +0,0 @@
-package audio
-
-// Audio encoder/decoder interface.
-// Used to encode raw PCM Frames to an encoded frame,
-// and decode those frames back to PCM frames
-type EncoderDecoder interface {
-	Encode([]PCMFrame) []EncodedFrame
-	Decode([]EncodedFrame) []PCMFrame
-}
---
+++ internal/encoderdecoder/encoderdecoder.go
@@ -0,0 +1,31 @@
+package encoderdecoder
+
+import (
+	"errors"
+
+	"github.com/hmcalister/roundtable/internal/frame"
+	"github.com/pion/webrtc/v4"
+)
+
+var (
+	errEncoderDecoderTypeNotImplemented = errors.New("specified encoderdecoder type is not implemented")
+)
+
+// Audio encoder/decoder interface.
+// Used to encode raw PCM Frames to an encoded frame,
+// and decode those frames back to PCM frames
+type EncoderDecoder interface {
+	Encode([]frame.PCMFrame) []frame.EncodedFrame
+	Decode([]frame.EncodedFrame) []frame.PCMFrame
+}
+
+// Create a new encoder/decoder based on the negotiated codec
+// If something goes wrong during creation of an encoder/decoder
+// (e.g. the mime type does not have an implementation) then a nil Encoder/Decoder
+// and an error is returned.
+func NewEncoderDecoder(codec webrtc.RTPCodecCapability) (*EncoderDecoder, error) {
+	switch codec.MimeType {
+	default:
+		return nil, errEncoderDecoderTypeNotImplemented
+	}
+}
