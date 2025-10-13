f52f06bded12224a66569f3fd4e3dda88f4a2dfb
Hayden McAlister <haydenmcalister49@gmail.com>
2025-10-14 00:35:10 +1300

    Move device interfaces into audiodevice package for better separation of concerns

--- internal/audiodevice/device.go
+++ internal/audiodevice/device.go
@@ -1,7 +1,6 @@
-package device
+package audiodevice
 
-// The number of items to buffer in each stream
-const DEVICE_STREAM_BUFFER_SIZE = 128
+import "github.com/hmcalister/roundtable/internal/audio"
 
 // Interface for audio input devices, e.g. microphones
 //
@@ -11,7 +10,7 @@ type AudioInputDevice interface {
 	// Get the input stream of this audio device.
 	//
 	// Raw audio data (as PCMFrames) will arrive on the returned channel.
-	GetStream() <-chan []int16
+	GetStream() <-chan audio.PCMFrame
 	NumChannels() int
 	SampleRate() int
 }
@@ -19,11 +18,11 @@ type AudioInputDevice interface {
 // Interface for audio output devices, e.g. speakers
 //
 // Output devices need only define some way to consume data,
-// taken as a channel (stream) of PCMFrames
+// taken as a channel (stream) of audio.PCMFrames
 type AudioOutputDevice interface {
 	// Set the data stream of this audio device.
 	//
 	// Raw audio data (as PCMFrames) will arrive on the given channel
 	// and should be passed meaningfully be the output device (e.g. sent to speakers).
-	SetStream(chan<- []int16)
+	SetStream(chan<- audio.PCMFrame)
 }
