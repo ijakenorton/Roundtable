ceb21fb45e363eef7acfca71fa369dc36370beb3
Hayden McAlister <haydenmcalister49@gmail.com>
2025-10-14 02:39:02 +1300

    Update encoderdecoder interface to return individual frames and also errors
    This better suits the idea of what an encoder/decoder does, as encoding
    or decoding may fail

--- internal/encoderdecoder/encoderdecoder.go
+++ internal/encoderdecoder/encoderdecoder.go
@@ -15,15 +15,15 @@ var (
 // Used to encode raw PCM Frames to an encoded frame,
 // and decode those frames back to PCM frames
 type EncoderDecoder interface {
-	Encode([]frame.PCMFrame) []frame.EncodedFrame
-	Decode([]frame.EncodedFrame) []frame.PCMFrame
+	Encode(pcmData frame.PCMFrame) (frame.EncodedFrame, error)
+	Decode(encodedData frame.EncodedFrame) (frame.PCMFrame, error)
 }
 
 // Create a new encoder/decoder based on the negotiated codec
 // If something goes wrong during creation of an encoder/decoder
 // (e.g. the mime type does not have an implementation) then a nil Encoder/Decoder
 // and an error is returned.
-func NewEncoderDecoder(codec webrtc.RTPCodecCapability) (*EncoderDecoder, error) {
+func NewEncoderDecoder(codec webrtc.RTPCodecCapability) (EncoderDecoder, error) {
 	switch codec.MimeType {
 	default:
 		return nil, errEncoderDecoderTypeNotImplemented
