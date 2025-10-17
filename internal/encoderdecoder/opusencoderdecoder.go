79e4c320b266d3c236189081bb6f81184b80578a
Hayden McAlister <haydenmcalister49@gmail.com>
2025-10-18 05:34:26 +1300

    Move encoder decoders back to an internal package

--- internal/encoderdecoder/opusencoderdecoder.go
+++ internal/encoderdecoder/opusencoderdecoder.go
@@ -2,6 +2,7 @@ package encoderdecoder
 
 import (
 	"errors"
+	"log/slog"
 	"time"
 
 	"github.com/Honorable-Knights-of-the-Roundtable/roundtable/pkg/frame"
@@ -129,13 +130,13 @@ func (encdec *OpusEncoderDecoder) Encode(pcmData frame.PCMFrame) ([]frame.Encode
 		// We *could* handle in parts, but instead just return an error.
 		// TODO: Handle massive PCM frames.
 
-		// slog.Debug(
-		// 	"pcm frame too large",
-		// 	"pcmFrame length", len(pcmData),
-		// 	"pcmFrameBuffer length", len(encdec.pcmFrameBuffer),
-		// 	"encdec.pcmFrameBufferHead", encdec.pcmFrameBufferHead,
-		// 	"encdec.pcmFrameBufferTail", encdec.pcmFrameBufferTail,
-		// )
+		slog.Debug(
+			"pcm frame too large",
+			"pcmFrame length", len(pcmData),
+			"pcmFrameBuffer length", len(encdec.pcmFrameBuffer),
+			"encdec.pcmFrameBufferHead", encdec.pcmFrameBufferHead,
+			"encdec.pcmFrameBufferTail", encdec.pcmFrameBufferTail,
+		)
 
 		return nil, errors.New("pcm frame len larger than pcmFrameBuffer")
 	}
@@ -184,13 +185,13 @@ func (encdec *OpusEncoderDecoder) Encode(pcmData frame.PCMFrame) ([]frame.Encode
 		encdec.pcmFrameBufferHead += encdec.encodingFrameSize
 	}
 
-	// slog.Debug("encoding finished",
-	// 	"incomingDataLen", len(pcmData),
-	// 	"pcmFrameBufferHead", encdec.pcmFrameBufferHead,
-	// 	"pcmFrameBufferTail", encdec.pcmFrameBufferTail,
-	// 	"encodedFrameBufferTail", encdec.encodedFrameBufferTail,
-	// 	"numEncodedFrames", numEncodedFrames,
-	// )
+	slog.Debug("encoding finished",
+		"incomingDataLen", len(pcmData),
+		"pcmFrameBufferHead", encdec.pcmFrameBufferHead,
+		"pcmFrameBufferTail", encdec.pcmFrameBufferTail,
+		"encodedFrameBufferTail", encdec.encodedFrameBufferTail,
+		"numEncodedFrames", numEncodedFrames,
+	)
 	return encdec.encodedFrameReturnBuffer[:numEncodedFrames], nil
 }
 
