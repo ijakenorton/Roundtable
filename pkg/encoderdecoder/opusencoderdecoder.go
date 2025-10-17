9b4484e02ecd5901f4a0bc36fb77170dbb02a349
Hayden McAlister <haydenmcalister49@gmail.com>
2025-10-18 05:15:14 +1300

    Add valid OPUS frame durations as constant enums

--- pkg/encoderdecoder/opusencoderdecoder.go
+++ pkg/encoderdecoder/opusencoderdecoder.go
@@ -2,15 +2,26 @@ package encoderdecoder
 
 import (
 	"errors"
+	"time"
 
 	"github.com/Honorable-Knights-of-the-Roundtable/roundtable/pkg/frame"
 	"github.com/jj11hh/opus"
 )
 
-// TODO:
-// Is it okay to have a static frame for encoding and decoding into?
-// Will this get overwritten before being consumed?
-// Better to have a ring buffer of frames to use?
+// Valid frame durations for OPUS encoding
+//
+// Longer frame durations introduce more latency, but are more bandwidth-efficient and potentially higher quality
+type OPUSFrameDuration time.Duration
+
+const (
+	OPUS_FRAME_DURATION_2_POINT_5_MS OPUSFrameDuration = OPUSFrameDuration(2500 * time.Microsecond)
+	OPUS_FRAME_DURATION_5_MS         OPUSFrameDuration = OPUSFrameDuration(5 * time.Millisecond)
+	OPUS_FRAME_DURATION_10_MS        OPUSFrameDuration = OPUSFrameDuration(10 * time.Millisecond)
+	OPUS_FRAME_DURATION_20_MS        OPUSFrameDuration = OPUSFrameDuration(20 * time.Millisecond)
+	OPUS_FRAME_DURATION_40_MS        OPUSFrameDuration = OPUSFrameDuration(40 * time.Millisecond)
+	OPUS_FRAME_DURATION_60_MS        OPUSFrameDuration = OPUSFrameDuration(60 * time.Millisecond)
+	OPUS_FRAME_DURATION_120_MS       OPUSFrameDuration = OPUSFrameDuration(120 * time.Millisecond)
+)
 
 type OpusEncoderDecoder struct {
 	sampleRate  int
