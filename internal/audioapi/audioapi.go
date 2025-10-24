bbdee91234b624e86d0454dcd842de5d8f596c9f
Hayden McAlister <haydenmcalister49@gmail.com>
2025-10-25 04:30:30 +1300

    Add an interface for an audioIODeviceAPI to query devices, set devices, etc

---
+++ internal/audioapi/audioapi.go
@@ -0,0 +1,39 @@
+package audioapi
+
+import "github.com/Honorable-Knights-of-the-Roundtable/roundtable/pkg/audiodevice"
+
+type AudioIODeviceID struct {
+	// The ID of the device
+	//
+	// Should come from the underlying API (RTAudio/PortAudio),
+	// But could be defined in some programmatic way by the AudioIODeviceAPI
+	//
+	// Intended to be the canonical way to reference the AudioIODevice
+	// (e.g. a microphone or speaker), such that when telling the API
+	// to use a device as the default input/output, it is this value
+	// that is used to identify the device.
+	ID int
+
+	// A human-readable name for the device, if one exists.
+	// Not necessary, and not canonical.
+	Name string
+
+	// The device properties (sample rate and channels) of this device.
+	// Note that Roundtable only supports devices with mono or stereo channels.
+	DeviceProperties audiodevice.DeviceProperties
+}
+
+// Define an API to interface with hardware devices.
+// Intended to be an abstract way to:
+// - Query existing devices (input and output)
+// - Initialize an input/output device as an AudioSourceDevice/AudioSinkDevice respectively
+//
+// Implementations could include small wrappers around RTAudio and PortAudio
+type AudioIODeviceAPI interface {
+	GetAllInputDevices() []AudioIODeviceID
+	GetInputDevice(AudioIODeviceID) (audiodevice.AudioSourceDevice, error)
+	GetDefaultInputDevice() (audiodevice.AudioSourceDevice, error)
+	GetAllOutputDevices() []AudioIODeviceID
+	GetOutputDevice(AudioIODeviceID) (audiodevice.AudioSinkDevice, error)
+	GetDefaultOutputDevice() (audiodevice.AudioSinkDevice, error)
+}
